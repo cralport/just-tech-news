77c7faf2d6b1d609350f6ab66f4be053a0497a9c
cory alport <cralport@gmail.com>
2021-04-20 18:38:17 -0600

    started handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/comments', commentRoutes);
 
 router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
